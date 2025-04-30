7d5dc97d1886ac2a453512c51f1ccb9a4197f907
Cerqueira <viccerqueirabarros@gmail.com>
2025-04-30 13:27:18 -0300

    correção na falha do enviar dados

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const axios = require("axios");
 
 const app = express();
 
+const webhookUrl = process.env.WEBHOOK_URL;
+
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,12 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files
 app.use(express.static(path.join(__dirname, 'src/public')));
 
-// Importar rota do comprovante
-const nuComprovanteRouter = require('./routes/nu-comprovante');
-
-// Rotas
-app.use('/nu-comprovante', nuComprovanteRouter);
-
+// Rota para receber localização e enviar para o webhook
 app.post("/send-location", async (req, res) => {
   const {
     latitude,
@@ -57,12 +55,17 @@ app.post("/send-location", async (req, res) => {
 
   try {
     // Envia os dados para o webhook
-    const response = await axios.post('https://webhookauto.tampus.chat/webhook/send', message);
+    const response = await axios.post(webhookUrl, message);
 
+    // Se a requisição for bem-sucedida
+    res.status(200).json({ success: true, message: 'Dados enviados com sucesso.' });
   } catch (error) {
+    console.error('Erro ao enviar dados para o webhook:', error);
+    res.status(500).json({ success: false, message: 'Erro ao enviar dados para o webhook.' });
   }
 });
 
+// Inicializando o servidor
 const PORT = process.env.PORT || 8088;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
